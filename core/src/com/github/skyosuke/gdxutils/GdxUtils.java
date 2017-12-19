65356b8d77c0a8a698a73cf400bcf00e35008bb3
S.Kyousuke <surasek@gmail.com>
2017-12-19 23:03:40 +0700

    clean up, update version to 0.0.5

--- core/src/com/github/skyosuke/gdxutils/GdxUtils.java
+++ core/src/com/github/skyosuke/gdxutils/GdxUtils.java
@@ -2,7 +2,6 @@ package com.github.skyosuke.gdxutils;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -11,8 +10,8 @@ public class GdxUtils extends ApplicationAdapter {
 
     TextureAtlas playerAtlas;
     SpriteBatch batch;
-
     AnimatedRegion animatedRegion;
+
     @Override
     public void create() {
         playerAtlas = new TextureAtlas(Gdx.files.internal("player.atlas"));
@@ -24,40 +23,6 @@ public class GdxUtils extends ApplicationAdapter {
     public void render() {
         Gdx.gl.glClearColor(0, 0, 0, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        if (Gdx.input.isKeyJustPressed(Input.Keys.F1)) {
-            animatedRegion.setAnimation("stand");
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.F2)) {
-            animatedRegion.setAnimation("run");
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.F3)) {
-            animatedRegion.setAnimation("jump");
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.F4)) {
-            animatedRegion.setAnimation("hit");
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.F5)) {
-            animatedRegion.setPlaybackScale(1.0f);
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.F6)) {
-            animatedRegion.setPlaybackScale(0.5f);
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.F7)) {
-            animatedRegion.setPlaybackScale(2.0f);
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.P)) {
-            animatedRegion.pause();
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
-            animatedRegion.play();
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-            animatedRegion.stop();
-        }
-        if (animatedRegion.isAnimationEquals("hit") && animatedRegion.isAnimationFinished()) {
-            animatedRegion.setAnimation("run");
-        }
 
         animatedRegion.update(Gdx.graphics.getDeltaTime());
 
